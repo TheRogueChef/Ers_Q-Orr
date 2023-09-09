e4930277a2911d98a7ff44dc0b6c32d82c9aae31
Erik Carlson <theroguechef@comcast.net>
2023-09-08 18:01:52 -0700

    added affiliates names

--- src/components/Affiliations.js
+++ src/components/Affiliations.js
@@ -15,7 +15,7 @@ import Leia from '../images/LeiaSexy.png';
 const Affiliations = () => {
     return (
         <div className='affShell'  >
-            <h1 style={{ fontFamily: 'PersianSword' }}>Affiliations</h1>
+            <h1 style={{ fontFamily: 'PersianSword' }}>Affiliates</h1>
             <Link className='homeLink2' to='/'>Home</Link>
             <br /><br /><br />
             <div className='droidText' >
@@ -30,8 +30,8 @@ const Affiliations = () => {
                         </FullScreenImage>
                     </div>
                     <div>
-                        <p>T.A.D.</p>
-                        <p>( TU Aqua Droid )</p>
+                        <p>Bossk</p>
+                        <p>Bounty Hunter</p>
                         <p className='droidBlurb'>TAD is one of my favorites. These battle droids are easy to program and worthy in battle. They dont have a much of a personality and can be a bit repetitive. But, he is still with me and although he has no sense of humor, he is someone I enjoy having around even if he is terrible with ship maintenance.</p>
                         </div>
 
@@ -43,8 +43,8 @@ const Affiliations = () => {
                         <Image src={Leia} alt='...' />
                     </FullScreenImage>
                     <div>
-                        <p>Rocco</p>
-                        <p>( Octuptarra combat tri~droid )</p>
+                        <p>Leia Organa</p>
+                        <p>Princess/Rebel Leader</p>
                         <p className='droidBlurb'>Rocco was my first droid. He came with The Dark Naught and was excellent with all ship maintenance and, well pretty much anything I needed. He was much faster after a few modifications and his voice modulator was set up to be that of a kima, which worked out well in so many ways. Losing him was tough. </p>
                     </div>
                 </div>
@@ -54,8 +54,8 @@ const Affiliations = () => {
                         <Image src={BobaBW} alt='...' />
                     </FullScreenImage>
                     <div>
-                        <p>R4</p>
-                        <p>( R4~I9 Agromech droid )</p>
+                        <p>Boba Fett</p>
+                        <p>Bounty Hunter</p>
                         <p className='droidBlurb'>R4 was a gift from a farmer on Tatooine. He was very good at communicating with other computers and being that he was quite manipulative he was able to convince a lot of systems to do whatever we needed. As far as his personality went, he was an asshole. He was always very rude and always had to have the last word. I have always appreciated his ability but I finally had to shut him down due to him harassing just about anyone who boarded.</p>
                     </div>
                 </div>
@@ -65,8 +65,8 @@ const Affiliations = () => {
                         <Image src={Cad} alt='...' />
                     </FullScreenImage>
                     <div>
-                        <p>Rocco</p>
-                        <p>( Octuptarra combat tri~droid )</p>
+                        <p>Cad Bane</p>
+                        <p>Bounty Hunter</p>
                         <p className='droidBlurb'>Rocco was my first droid. He came with The Dark Naught and was excellent with all ship maintenance and, well pretty much anything I needed. He was much faster after a few modifications and his voice modulator was set up to be that of a kima, which worked out well in so many ways. Losing him was tough. </p>
                     </div>
                 </div>
@@ -76,8 +76,8 @@ const Affiliations = () => {
                         <Image src={Han} alt='...' />
                     </FullScreenImage>
                     <div>
-                        <p>K2</p>
-                        <p>( KX series K~2SO Enforcer )</p>
+                        <p>Han Solo</p>
+                        <p>Smuggler/Rebel adjacent</p>
                         <p className='droidBlurb'>K2 was a very competent partner. His ability to pilot and to think on his feet were excellent. He was kind of a wet rag most of the time and was always feeling that he was very put upon to do just about anything. I have heard that most of these droids were very depressing. He is still out there somewhere, feeling sorry for himself I am sure.</p>
                     </div>
                 </div>
@@ -87,8 +87,8 @@ const Affiliations = () => {
                         <Image src={Darth} alt='...' />
                     </FullScreenImage>
                     <div>
-                        <p>Rocco</p>
-                        <p>( Octuptarra combat tri~droid )</p>
+                        <p>Darth Vader</p>
+                        <p>Sith Lord</p>
                         <p className='droidBlurb'>Rocco was my first droid. He came with The Dark Naught and was excellent with all ship maintenance and, well pretty much anything I needed. He was much faster after a few modifications and his voice modulator was set up to be that of a kima, which worked out well in so many ways. Losing him was tough. </p>
                     </div>
                 </div>
